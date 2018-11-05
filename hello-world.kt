2ede48b3c1fb964c18ee047005add031a7427506
Dhananjay <dhananjaymohnot@gmail.com>
2018-11-05 17:56:57 +0530

    hello world and variabledeclaration examples in kotlin

---
+++ hello-world.kt
@@ -0,0 +1,7 @@
+/**
+ * We declare a package-level funcation main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+fun main(args: Array<String>) {
+	println("Hello, world!")
+}
---
+++ variable-declaration.kt
@@ -0,0 +1,9 @@
+un main(args: Array<String>) {
+	var i: Int = 8
+	var f: Float = 657f
+	var b: Boolean = true
+
+	println("vale of i is" + i)
+	println("value of i is $i")
+	println(value of i is $(i + 2))
+}
